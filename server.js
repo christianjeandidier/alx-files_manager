110d1ee5e541b2c03236c9a7fad4eb436a833c89
christianjeandidier <ndo.jeandidier@gmail.com>
2024-12-11 16:24:20 -0800

    11. New user - welcome email

--- server.js
+++ server.js
@@ -1,10 +1,14 @@
 import express from 'express';
-import routes from './routes/index';
+import controllerRouting from './routes/index';
 
 const app = express();
 const port = process.env.PORT || 5000;
 
-app.use('/', routes);
+app.use(express.json());
+controllerRouting(app);
+
 app.listen(port, () => {
   console.log(`Server running on port ${port}`);
 });
+
+export default app;
